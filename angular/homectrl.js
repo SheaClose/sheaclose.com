e7691fdaf58928facd1c2442392bb821483ca54a
SheaClose <SheaClose@gmail.com>
2016-11-15 23:29:21 -0600

    projects page

--- angular/homectrl.js
+++ angular/homectrl.js
@@ -1,14 +1,3 @@
 angular.module("app")
 .controller("homeCtrl", function($scope, homeService){
-	let init = () => {
-		$(document).ready(function(){
-			$('.modal').modal();
-		});
-	}
-	$scope.modal = (src) => {
-		$scope.pictureSource = src;
-		$("modal1").modal('open');
-	}
-
-	init();
 })
