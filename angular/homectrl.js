dcb4a0ca80b1c10aff697e5da4dc5e692d1b5872
SheaClose <SheaClose@gmail.com>
2016-11-16 11:06:22 -0600

    projects page complete

--- angular/homectrl.js
+++ angular/homectrl.js
@@ -1,3 +1,30 @@
 angular.module("app")
 .controller("homeCtrl", function($scope, homeService){
+
+	$scope.modal = function(num) {
+		$(`#modal${num}`).modal('open');
+	}
+
+	$(document).ready(function(){
+		$('.modal').modal();
+	});
+
+	$scope.alienHunt = true;
+	$scope.dogCompany = false;
+	$scope.bender = false;
+
+	$scope.changePage = (page) => {
+		$scope.alienHunt = false;
+		$scope.dogCompany = false;
+		$scope.bender = false;
+		if (page === "alienHunt") {
+			$scope.alienHunt = true;
+		}
+		else if (page === "dogCompany") {
+			$scope.dogCompany = true;
+		}
+		else if (page === "bender") {
+			$scope.bender = true;
+		}
+	}
 })
