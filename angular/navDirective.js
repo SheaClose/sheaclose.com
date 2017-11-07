4dcc0df86d7472543dd75ab329115464932ec2cd
SheaClose <SheaClose@gmail.com>
2017-11-06 19:57:59 -0600

    Getting rid of the Blog

--- angular/navDirective.js
+++ angular/navDirective.js
@@ -1,12 +1,12 @@
 export default function() {
-	return {
-template: `<div class="page-footer light-green darken-3">
+  return {
+    template: `<div class="page-footer light-green darken-3">
 						<div class="row">
 							<a class="col s2 m2 l2 green-text text-lighten-3 center-align" ui-sref="home">home</a>
 							<a class="col s2 m2 l2 green-text text-lighten-3 center-align" ui-sref="about">about</a>
 							<a class="col s2 m2 l2 green-text text-lighten-3 center-align" ui-sref="projects">projects</a>
 							<a class="col s2 m2 l2 green-text text-lighten-3 center-align" ui-sref="personal">personal</a>
-              <a class="col s2 m2 l2 green-text text-lighten-3 center-align" ui-sref="blog">blog</a>
+              <a class="col s2 m2 l2 green-text text-lighten-3 center-align"></a>
 							<div class="col s2 m2 l2 grey-text text-lighten-4 center-align soc_logo_container" href="#!">
 								<a class="" href="https://twitter.com/SheaClose"><img class="soc-logo" src="Assets/icons/twitter-logo copy.png"/></a>
 								<a class="" href="https://www.linkedin.com/in/sheaclose"><img class="soc-logo" src="Assets/icons/linkedin-logo copy.png"/></a>
@@ -14,5 +14,5 @@ template: `<div class="page-footer light-green darken-3">
 							</div>
 						</div>
 					</div>`
-	}
+  };
 }
--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const   express = require('express')
- 				, cors = require('cors')
- 				, bodyParser = require('body-parser')
- 				, port = 3000
- 				, app = express()
-        , massive = require('massive')
-        , config = require('./server/config')
+const express = require("express"),
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  port = 3005,
+  app = express(),
+  massive = require("massive"),
+  config = require("./server/config");
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 app.use("/", express.static(__dirname));
-massive(config.postgres).then(function(dbInstance){
-  console.log(dbInstance);
-  app.set('db', dbInstance)
-}).catch(err=>console.log(err))
+massive(config.postgres)
+  .then(function(dbInstance) {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
 
-
-app.post('/api/blog', function(req,res){
-  var {title, body} = req.body;
+app.post("/api/blog", function(req, res) {
+  var { title, body } = req.body;
   var date = Date();
-  req.app.get('db')
-    .run('insert into blogs (title, body, date) values ($1, $2, $3); select * from blogs'
-    , [title, body, date])
+  req.app
+    .get("db")
+    .run(
+      "insert into blogs (title, body, date) values ($1, $2, $3); select * from blogs",
+      [title, body, date]
+    )
     .then(blog => {
-      console.log(blog);
-      return res.status(200).json(blog)
+      return res.status(200).json(blog);
     })
-    .catch((err) => {
-      return res.status(500).json(err)
-    })
-})
+    .catch(err => {
+      return res.status(500).json(err);
+    });
+});
 
-app.get('/api/blog', function(req, res){
-  req.app.get('db').run('select * from blogs')
-  .then((response) => {
-    return res.status(200).json(response);
-  })
-  .catch((err) => {
-    return res.status(500).json(err)
-  })
-})
+app.get("/api/blog", function(req, res) {
+  req.app
+    .get("db")
+    .run("select * from blogs")
+    .then(response => {
+      return res.status(200).json(response);
+    })
+    .catch(err => {
+      return res.status(500).json(err);
+    });
+});
 
 app.listen(port, function() {
-  console.log('Server listening on port', port)
-})
+  console.log("Server listening on port", port);
+});
